d7a141a3fb0d9864ab33b548a3399c909a99f936
MiranValincic <80262902+MiranValincic@users.noreply.github.com>
2021-11-16 17:43:46 +0100

    Implemented for loop and condition

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,37 @@ let pokemonList = [
     name: "Squirtle",
     height: 0.5,
     types: ['monster', 'water']
-}
+},
+
+{
+    name: "Venusaur",
+    height: 2,
+    types: ['monster', 'grass']
+},
+
+{
+    name: "Charizard",
+    height: 1.7,
+    types: ['monster', 'dragon']
+},
+
+{
+    name: "Blastoise",
+    height: 1.6,
+    types: ['monster', 'water']
+},
     
 ] ;
+
+// Write Pokemon names on website and looks for a Pokemon with the high > 1
+
+for (let i=0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1){
+        document.write(pokemonList[i].name + " " +  '(' + 'height:' + " " + pokemonList[i].height + ')' + " " + '-'  +  " " +  `Wow that's big` + '<br>' );
+    } else if (pokemonList[i].height < 1){
+        document.write(pokemonList[i].name + " " + '(' + 'height:' + " " +pokemonList[i].height + ')' + '<br>' );
+    } 
+}
+
+
 
