b43508b2dc8a16a4bac0367c0be16612da231061
MiranValincic <80262902+MiranValincic@users.noreply.github.com>
2021-11-25 12:36:47 +0100

    Updated task

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 // IIFE function
 let pokemonRepository = (function () {
-  const list = [
-    ["Bulbasor", 0.7, ["grass", "poison"]],
-    ["Charmander", 0.6, ["monster", "dragon"]],
-    ["Squirtle", 0.5, ["monster", "water"]],
-    ["Venusaur", 2, ["monster", "grass"]],
-    ["Charizard", 1.7, ["monster", "dragon"]],
-    ["Blastoise", 1.6, ["monster", "water"]],
-  ];
-
   const pokemonList = [];
-  list.forEach(function (pokemon) {
-    const name = pokemon[0];
-    const height = pokemon[1];
-    const types = pokemon[2];
-    add(name, height, types);
-  });
 
   function getAll() {
     return pokemonList;
@@ -37,14 +22,31 @@ let pokemonRepository = (function () {
 
 // Add new pokemon
 
-pokemonRepository.add("Pikachu", "0.4", ["Field", "Fairy"]);
+const list = [
+  ["Bulbasor", 0.7, ["grass", "poison"]],
+  ["Charmander", 0.6, ["monster", "dragon"]],
+  ["Squirtle", 0.5, ["monster", "water"]],
+  ["Venusaur", 2, ["monster", "grass"]],
+  ["Charizard", 1.7, ["monster", "dragon"]],
+  ["Blastoise", 1.6, ["monster", "water"]],
+  ["Pikachu", 0.4, ["Field", "Fairy"]],
+];
+
+list.forEach(function (pokemon) {
+  const name = pokemon[0];
+  const height = pokemon[1];
+  const types = pokemon[2];
+  pokemonRepository.add(name, height, types);
+});
 
 var temp = "<ul>";
 pokemonRepository.getAll().forEach(function (pokemon) {
+  const height = pokemon.height;
+  const isBig = height > 1 ?  " - Wow that's big!" : " ";
   temp += `
   <li>
     ${pokemon.name} <br />
-    Height: ${pokemon.height} <br />
+    Height: ${pokemon.height} ${isBig} <br />
     Types: ${pokemon.types}
   </li>`;
 });
